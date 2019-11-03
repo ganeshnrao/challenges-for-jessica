72ae1e5a45ea59dcb74d861fc09f57465eed397a
Ganesh Rao <ganeshnrao@me.com>
2019-11-02 23:22:01 -0700

    Write up instructions and create first challenge

---
+++ day-01.js
@@ -0,0 +1,60 @@
+// node day-01.js - should output ten random names as shown below.
+// 01. Pamela Clements
+// 02. Alan Hurst
+// 03. Joe Reilly
+// 04. Stephanie Bentley
+// 05. Wayne Booker
+// 06. Carol Tate
+// 07. Jeremy Houston
+// 08. Jack Lucas
+// 09. Linda Riggs
+// 10. Arthur Frost
+
+const names = require("./assets/names.json");
+
+/**
+ * @param  {string[]} firstNames - array of strings of first names
+ * @param  {string[]} lastNames - array of strings of last names
+ * @returns {string} - a random generated full name, e.g. Alan Hurst, Wayne Booker etc.
+ */
+function generateOneFullName(firstNames, lastNames) {
+  // TODO: Implement
+}
+
+/**
+ * @param  {number} count - number of names to generate
+ * @param  {string[]} firstNames - array of strings of first names
+ * @param  {string[]} lastNames - array of strings of last names
+ * @returns {string[]} - array of random full names as strings
+ */
+function generateFullNames(count, firstNames, lastNames) {
+  // TODO: Implement
+  return [];
+}
+
+/**
+ * @param  {string[]} fullNames - array of strings of full names
+ * @description logs the given full names with serial numbers prefixes
+ */
+function logWithSeriallNumbers(fullNames) {
+  if (fullNames && fullNames.length) {
+    const slNumberLength = String(fullNames.length).length;
+    fullNames.forEach(function(name, i) {
+      const slNumber = String(i + 1).padStart(slNumberLength, "0");
+      console.log(`${slNumber}. ${name}`);
+    });
+  } else {
+    console.error(`{ No names given }`);
+  }
+}
+
+if (require.main === module) {
+  const fullNames = generateFullNames(10, names.first, names.last);
+  logWithSeriallNumbers(fullNames);
+}
+
+module.exports = {
+  generateOneFullName,
+  generateFullNames,
+  printNames: logWithSeriallNumbers
+};
---
+++ readme.md
@@ -0,0 +1,13 @@
+Dear Jessica,
+
+This repository is for you! I hope you will enjoy tackling these challenges and learn to write some functions in Javascript. Try to tackle one problem everyday. To get started, clone this repository and follow these steps.
+
+1. Create a separate branch for each challenge and name your branch as `day-<number>`, e.g. `day-01` for the first day&rsquo;s challenge.
+2. Work on the file with the name of the day, e.g. `day-01.js` on the branch for that first day.
+3. All of the challenges here will be using Node. So you can run them easily in the terminal using the command `node` e.g. `node day-01.js` to run the first day&rsquo; challenge.
+4. Once you&rsquo;ve solved the challenge, or most of it, create a pull-request to the `master` branch.
+
+In the challenge files, above each function, you will see a comment specifying the function&rsquo;s  inputs and return values. You may not change these. Your task is to fill in the functions with code so that the returns the values as defined by the specification.
+
+Love,
+:heart Ganesh
