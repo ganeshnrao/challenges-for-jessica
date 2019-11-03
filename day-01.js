c27158fd203180475b23ca4722639527727dc28f
Ganesh Rao <ganeshnrao@me.com>
2019-11-03 12:18:35 -0800

    Add detailed comment and simplify requirement to only one method

--- day-01.js
+++ day-01.js
@@ -1,42 +1,36 @@
-// node day-01.js - should output ten random names as shown below.
-// 01. Pamela Clements
-// 02. Alan Hurst
-// 03. Joe Reilly
-// 04. Stephanie Bentley
-// 05. Wayne Booker
-// 06. Carol Tate
-// 07. Jeremy Houston
-// 08. Jack Lucas
-// 09. Linda Riggs
-// 10. Arthur Frost
+/**
+ * Generate random full names
+ *
+ * You are given an array of 200 most common first names and another array of
+ * 1000 most common last names. Your task is to implement a function called
+ * "generateFullName" which takes those two arrays as inputs and generates a
+ * random full name.
+ *
+ * Note that there some names that might appear in both the first names array
+ * and last names array, so take care to avoid names where the first and last
+ * names are the same, e.g. "James James", "Michael Michael" should be avoided.
+ */
 
 const names = require("./assets/names.json");
-const { logWithSerialNumbers, runIfMain } = require("./assets/utils");
+const { runIfMain } = require("./assets/utils");
 
 /**
  * @param  {string[]} firstNames - array of strings of first names
  * @param  {string[]} lastNames - array of strings of last names
  * @returns {string} - a random generated full name, e.g. Alan Hurst, Wayne Booker etc.
  */
-function generateOneFullName(firstNames, lastNames) {
-  // TODO: Implement
-}
-
-/**
- * @param  {number} count - number of names to generate
- * @param  {string[]} firstNames - array of strings of first names
- * @param  {string[]} lastNames - array of strings of last names
- * @returns {string[]} - array of random full names as strings
- */
-function generateFullNames(count, firstNames, lastNames) {
+function generateFullName(firstNames, lastNames) {
   // TODO: Implement
 }
 
 runIfMain(module, function() {
-  const fullNames = generateFullNames(10, names.first, names.last);
-  logWithSerialNumbers(fullNames);
+  console.log(generateFullName(names.first, names.last));
+  console.log(generateFullName(names.first, names.last));
+  console.log(generateFullName(names.first, names.last));
+  console.log(generateFullName(names.first, names.last));
+  console.log(generateFullName(names.first, names.last));
 });
 
 module.exports = {
-  generateOneFullName
+  generateFullName
 };
